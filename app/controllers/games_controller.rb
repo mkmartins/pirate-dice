b60e777fdfdbd5d5e16be3be8525b05fd067c3e1
Marcos Martins <marcos.martins2307@gmail.com>
2014-09-17 20:44:40 -0600

    Adds a new controller message

--- app/controllers/games_controller.rb
+++ app/controllers/games_controller.rb
@@ -44,7 +44,7 @@ class GamesController < ApplicationController
   def update
     respond_to do |format|
       if @game.update(game_params)
-        format.html { redirect_to @game, notice: 'No one can save Tokyo' }
+        format.html { redirect_to @game, notice: 'Fight Like a PIRATE' }
         format.json { render :show, status: :ok, location: @game }
       else
         format.html { render :edit }
